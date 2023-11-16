4fd4ee8403a3955bf34ab14b0cc57f77a95d1b73
sakshatl <voteforsakshat@gmail.com>
2023-11-16 09:04:52 +0530

    Tic Tac Toe game render using JavaScript

--- 2-tic-tac-toe-game/app.js
+++ 2-tic-tac-toe-game/app.js
@@ -1,30 +1,88 @@
-const cells = document.querySelectorAll('.cell');
+const root = document.getElementById('root');
 
-const POSSIBLE_WINNING_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
+const BOARD = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
 ];
 
 let isXSelected = true;
 
-Array.from(cells).forEach((cell) => {
-  cell.addEventListener('click', () => {
-    if(isXSelected) {
-      cell.innerHTML = "x"
-    } else {
-      cell.innerHTML = "o";
-    }
-    // 1. Check for win
+function handleCellClick(e, row, col) {
+  // Put in value
+  if(isXSelected) {
+    e.target.innerHTML = "X";
+    BOARD[row][col] = "X";
+  } else {
+    e.target.innerHTML = "O";
+    BOARD[row][col] = "O";
+  }
 
-    // 2. Check for draw
-    
-    // 3. Switch sides
-    isXSelected = !isXSelected;
-  }, { once: true });
-});
+  // Check for win
+  checkForWin();
+
+  // Check for draw
+
+  // Switch sides
+  isXSelected = !isXSelected;
+}
+
+function checkForWin() {
+   // Check rows and columns
+  for (let i = 0; i < 3; i++) {
+    if (
+      BOARD[i][0] !== null &&
+      BOARD[i][0] === BOARD[i][1] &&
+      BOARD[i][1] === BOARD[i][2]
+    ) {
+      return true; // Row win
+    }
+
+    if (
+      BOARD[0][i] !== null &&
+      BOARD[0][i] === BOARD[1][i] &&
+      BOARD[1][i] === BOARD[2][i]
+    ) {
+      return true; // Column win
+    }
+  }
+
+  // Check diagonals
+  if (
+    BOARD[0][0] !== null &&
+    BOARD[0][0] === BOARD[1][1] &&
+    BOARD[1][1] === BOARD[2][2]
+  ) {
+    return true; // Diagonal win
+  }
+
+  if (
+    BOARD[0][2] !== null &&
+    BOARD[0][2] === BOARD[1][1] &&
+    BOARD[1][1] === BOARD[2][0]
+  ) {
+    return true; // Diagonal win
+  }
+
+  return false; // No win
+}
+
+function renderBoardUI() {
+  for (let row = 0; row < BOARD.length; row++) {
+    const rowElement = document.createElement('div');
+    rowElement.classList.add('row');
+    for (let col = 0; col < BOARD.length; col++) {
+      const cellElement = document.createElement('div');
+      cellElement.classList.add('cell');
+      cellElement.addEventListener('click', (e) => handleCellClick(e, row, col), { once: true });
+      rowElement.appendChild(cellElement);
+    }
+    root.appendChild(rowElement);
+  }
+}
+
+renderBoardUI();
+
+
+
+
